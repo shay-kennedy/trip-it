7b08587e9544826a737c8879f02828eeda192b64
R. Terrado <r.terrado@gmail.com>
2016-07-28 18:06:17 -0700

    Afternoon update

--- js/app.js
+++ js/app.js
@@ -2,22 +2,43 @@ $(document).ready(function() {
 
 	URL = 'https://api.foursquare.com/v2/venues/explore';
 
-	function getRequest(searchTerm) {
+	function getRequest(userInput, sectionType) {
 		var params = {
-			near: 'Phoenix, AZ',
-			section: 'restaurant',
+			near: userInput,
+			query: sectionType,
 			limit: 5,
+			radius: 500,
 			v: Date.now(),
 			client_id: 'QIWNWWTNLDX5SILZLFP0U4RDMQTSRVVN02YVDLAZC5TH5OVH',
 			client_secret: 'FHBPGACKE12LS2PMTJK54JNR3UESGC0PPIDWWSMS1UFAKA1J',
 		};
 		
 		$.getJSON(URL, params, function(data) {
-			console.log(data);
+			showDestination(data);
 		})
 
 	};
 
-	getRequest();
+	$('#location-form').submit(function(e){
+		e.preventDefault();
+		var userInput = $('#location-form input').val();
+		var sectionType = 'restuarant';
+		getRequest(userInput, sectionType);
+	})
+
+	var showDestination = function(data) {
+
+	// clone our result template code
+	//var result = $('.result .destination').clone();
+
+	$.each(data, function(index, data){
+		//$('.destination-name').text(data.response.groups[0].items[index].venue.name);
+		console.log(data.response.groups[0].items[index].venue.name);
+	});
+
+	// Set the question properties in result
+
+	//return result;
+};
 
 })
