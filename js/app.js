17b560deea51013be2e6ae5c0d70d2b522cbcb8a
shay-kennedy <kennedy.rshay@gmail.com>
2016-12-28 20:25:01 -0700

    Cleanup and commented

--- js/app.js
+++ js/app.js
@@ -4,6 +4,7 @@ $(document).ready(function() {
 
 	function getRequest(userInput, sectionType) {
 		var sectionName = sectionType;
+		// Params for API request
 		var params = {
 			near: userInput,
 			query: sectionType,
@@ -15,48 +16,58 @@ $(document).ready(function() {
 			client_secret: 'FHBPGACKE12LS2PMTJK54JNR3UESGC0PPIDWWSMS1UFAKA1J',
 		};
 
-
 		$.getJSON(URL, params, function(data) {
-   			 for (var i = 0; i < data.response.groups[0].items.length; i++) {
-   			 	console.log(data.response.groups[0].items[i].venue.name);
-   			 	var destination = $('#templates div.destination').clone();
-   			 	// destination.removeClass('destination');
+ 			console.log(data.response);
+ 			var results = data.response.groups[0].items;
+ 			console.log(results);
+ 			for (var i = 0; i < results.length; i++) {
+ 			 	// Cloning hidden html setup
+ 			 	var destination = $('#templates div.destination').clone();
 
-   			 	destination.find('.title a').text(data.response.groups[0].items[i].venue.name);
-   			 	destination.find('.title a').attr('href', 'https://foursquare.com/v/' + data.response.groups[0].items[i].venue.id);
-   			 	destination.find('.category').text(data.response.groups[0].items[i].venue.categories[0].name);
-// .attr()
+ 			 	// Setting location name, link, and category
+ 			 	destination.find('.title a')
+ 			 		.text(results[i].venue.name);
+ 			 	destination.find('.title a')
+ 			 		.attr('href', 'https://foursquare.com/v/' + results[i].venue.id);
+ 			 	destination.find('.category')
+ 			 		.text(results[i].venue.categories[0].name);
 
-   			 	if (data.response.groups[0].items[i].venue.photos.count == 1){
-   			 		destination.find('.card-image img').attr('src', data.response.groups[0].items[i].venue.photos.groups[0].items[0].prefix + '75x75' + data.response.groups[0].items[i].venue.photos.groups[0].items[0].suffix);
-   			 	}
-   			 	else {
-   			 		destination.find('.card-image img').attr('src', data.response.groups[0].items[i].venue.categories[0].icon.prefix + '88' + data.response.groups[0].items[i].venue.categories[0].icon.suffix);
-   			 	}
-				// $('#results-' + sectionName).append("<p><a href='https://foursquare.com/v/" + data.response.groups[0].items[i].venue.id + "' target='_blank'>" + data.response.groups[0].items[i].venue.name + "</a></p>");
-		        if (typeof data.response.groups[0].items[i].venue.rating !== "undefined"){
-		        	// $('#results-' + sectionName).append("<p>" + data.response.groups[0].items[i].venue.rating + "</p>");
-		        	destination.find('.rating').text(data.response.groups[0].items[i].venue.rating.toFixed(1));
-		       		destination.find('.rating').attr('style', 'background-color:#' + data.response.groups[0].items[i].venue.ratingColor);
-		        }
-		       	// $('#results-' + sectionName).append("<p>" + data.response.groups[0].items[i].venue.categories[0].name + "</p>");
-		        if (typeof data.response.groups[0].items[i].venue.price !== "undefined" ) {
-		        	// $('#results-' + sectionName).append("<p>Price: " + data.response.groups[0].items[i].venue.price.message + "</p>")
-	        		destination.find('.price').html('<b>Price:</b> ' + data.response.groups[0].items[i].venue.price.message);
-	        	}		       	
-		        if (typeof data.response.groups[0].items[i].tips !== "undefined"){
-		        	// $('#results-' + sectionName).append("<p>" + data.response.groups[0].items[i].tips[0].text + "</p>");
-		        	destination.find('.tip').html('<b>User Comment:</b> ' + data.response.groups[0].items[i].tips[0].text);
-		        }
-			  	$('#results-' + sectionName).append(destination);
-				}
-
-			})
-		};
+ 			 	// If location has photo use photo, if not use general category icon
+ 			 	if (results[i].venue.photos.count == 1){
+ 			 		destination.find('.card-image img')
+ 			 			.attr('src', results[i].venue.photos.groups[0].items[0].prefix + '75x75' + results[i].venue.photos.groups[0].items[0].suffix);
+ 			 	} else {
+ 			 		destination.find('.card-image img')
+ 			 			.attr('src', results[i].venue.categories[0].icon.prefix + '88' + results[i].venue.categories[0].icon.suffix);
+ 			 	}
+        
+ 			 	// If location has rating, diplay it
+        if (typeof results[i].venue.rating !== "undefined"){
+        	destination.find('.rating')
+        		.text(results[i].venue.rating.toFixed(1));
+       		destination.find('.rating')
+       			.attr('style', 'background-color:#' + results[i].venue.ratingColor);
+        }
+        
+        // If location has price, show price message
+        if (typeof results[i].venue.price !== "undefined" ) {
+      		destination.find('.price')
+      			.html('<b>Price:</b> ' + results[i].venue.price.message);
+      	}
+        
+        // If location has user tip, show tip
+        if (typeof results[i].tips !== "undefined"){
+        	destination.find('.tip')
+        		.html('<b>Visitor Tip:</b> ' + results[i].tips[0].text);
+        }
+		  	
+		  	$('#results-' + sectionName).append(destination);
+			}
+		})
+	};
 
 	$('#location-form').submit(function(e){
 		e.preventDefault();
-		//$('div.container').toggleClass('hidden', true);
 		$('.row div').html('');
 		var userInput = $('#location-form input').val();
 		getRequest(userInput, 'food');
